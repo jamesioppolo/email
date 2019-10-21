a27e45c1823e29e1e8efb946782586b7d34f5f6f
James Ioppolo <james.ioppolo@gmail.com>
2019-10-22 09:16:04 +1100

    removed random load balancing

--- MailController/mailController.js
+++ MailController/mailController.js
@@ -3,14 +3,8 @@ const sendgrid = require("../MailingSystems/sendgrid");
 
 module.exports = {
     send: (mailMessage, callback) => {
-        let client1, client2;
-        if (Math.random() < 0.5) {
-            client1 = mailgun;
-            client2 = sendgrid;
-        } else {
-            client1 = sendgrid;
-            client2 = mailgun;
-        }
+        let client1 = sendgrid;
+        let client2 = mailgun;
         client1.send(mailMessage, (client1Response) => {
             if (client1Response.statusCode === 200) {
                 callback(client1Response);        
